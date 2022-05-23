c890b0175209e8b855bbcce2c2337c77e8a2d002
Khush Mistry <mistryk22@hsnet.ahsd.org>
2022-05-23 09:37:30 -0400

    Initial commit

---
+++ app/src/main/java/com/mememaker/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.mememaker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.example.myapplication.R
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        templates.setOnClickListener{
+            val intent = (this, TemplatesPage:class)
+        }
+
+        }
+    }
+
