c8323fdeddadd09727330d16dc1bf03e44bc96c0
mistryk22 <khushbmistry@gmail.com>
2022-05-26 09:09:30 -0400

    Thursday second commit

--- app/src/main/java/com/mememaker/TemplatesPage.kt
+++ app/src/main/java/com/mememaker/TemplatesPage.kt
@@ -1,4 +1,15 @@
 package com.mememaker
 
-class TemplatesPage {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.R
+
+class TemplatesPage : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_templatespage)
+
+    }
 }
