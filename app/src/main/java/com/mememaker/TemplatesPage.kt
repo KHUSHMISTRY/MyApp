692def5f0cc5d91afcbff1a7fc0c597534daeb3a
mistryk22 <khushbmistry@gmail.com>
2022-05-31 09:52:02 -0400

    Tuesday first commit

--- app/src/main/java/com/mememaker/TemplatesPage.kt
+++ app/src/main/java/com/mememaker/TemplatesPage.kt
@@ -1,4 +1,29 @@
 package com.mememaker
 
-class TemplatesPage {
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.R
+
+class TemplatesPage : AppCompatActivity() {
+
+    @SuppressLint("WrongViewCast")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_templatespage)
+
+        val drakebutton = findViewById<Button>(R.id.draketemplatebutton)
+
+        drakebutton.setOnClickListener{
+
+            Toast.makeText(this,"Taking to drake template", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this,DrakeTemplatePage::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+    }
 }
